4180a4bfcb213e771d62d81e104a6183e57030bb
rohit mishra <rohit.mishra@raweng.com>
2018-10-29 11:35:59 +0530

    updated a reference doc

--- test/config.js
+++ test/config.js
@@ -2,11 +2,11 @@
 
 module.exports = {
     stack: {
-        "api_key": "bltc0a6a8609e24c651",
-        "access_token": "blt882b9ae3ee9af2e1",
-        "environment": "development"
+        "api_key": "<api_key>",
+        "access_token": "<Delivery_token>",
+        "environment": "<Environment>"
     },
-    host: "stag-cdn.contentstack.io",
+    host: "cdn.contentstack.io",
     url: "https://api.contentstack.io/v3",
     runscope: {
         url: "https://api.runscope.com/radar/76730163-fab9-4554-95c1-7ba75aaa2628/trigger?runscope_environment=1f5114ea-7a2d-4adf-9fd3-abc885c47783",
